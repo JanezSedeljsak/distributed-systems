2c20fa6d746ff13f85f94ecc6de446a58d39724e
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-11-17 13:53:29 +0100

    cleanup of dn3

--- DN3/divisors.c
+++ DN3/divisors.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
-#include "omp.h"
+#include <omp.h>
 
 #define MILLION 1000000 
-#define N (MILLION * 10)
-#define LINEAR
+#define N (MILLION)
 //#define PRINT
 
 int divsiors_sum(int n);
-
 int divisors[N + 1];
 
 /**
- * @brief Run with: gcc divisors.c -fopenmp -lm -O2 && time ./a.out
+ * @brief 
+ * Program is written so that first we build an array of diviors for each number, this way 
+ * we get constant look up for each number when looping. In the main loop we use reduction
+ * to sum up the "neighbour numbers" and in the end we print the elapsed time and result.
  * 
+ * (1 million - found 25275024)
+ * Run with: 
+ * gcc divisors.c -fopenmp -lm -O2 -o div.out
+ * srun --reservation=fri -n1 --cpus-per-task=16 div.out
+ * 
+ * @result
+ * Local: elapsed: 1.26
+ * NSC - 1: elapsed: 7.57
+ * NSC - 2: elapsed: 4.86
+ * NSC - 4: elapsed: 3.15
+ * NSC - 8: elapsed: 1.83
+ * NSC - 16: elapsed: 0.71
  */
+
 int main()
 {
     int start_time = omp_get_wtime();
@@ -29,7 +42,7 @@ int main()
     for (int i = 0; i <= N; i++)
     {
         // get current divisor for example for 220 get 284
-        int current_divisor = divisors[i];
+        int current_divisor = divsiors_sum(i);
         if (i < current_divisor && current_divisor <= N && divisors[current_divisor] == i)
         {
             #ifdef PRINT
